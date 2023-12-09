db3e7456d6ea3b9b541fd7dc9c168e2ec26cea29
Kassio <kassioborralho@gmail.com>
2023-12-08 23:04:30 -0300

    correcao de erro de conexao banco de dados

--- database/db.php
+++ database/db.php
@@ -1,15 +1,16 @@
 <?php
 
-$host = "";
-$username = "";
+$host = "localhost";
+$database = "db_exames";
+$username = "root";
 $password = "";
-$database = "";
 
 
 try {
     // Iniciar tentativa de conexao com o banco
     // string de conexao
-    $conexao = new PDO("mysql:host=$host;dbname=$database, $username, $password");
+    $conexao = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+
     $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $erro) {
     // Em caso de erro roda o que tem aqui
