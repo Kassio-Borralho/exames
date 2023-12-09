72651ea4b539beaff731c67f72b8064b19c598b9
Kassio <kassioborralho@gmail.com>
2023-12-08 22:49:49 -0300

    configurando base de dados

---
+++ database/db.php
@@ -0,0 +1,17 @@
+<?php
+
+$host = "";
+$username = "";
+$password = "";
+$database = "";
+
+
+try {
+    // Iniciar tentativa de conexao com o banco
+    // string de conexao
+    $conexao = new PDO("mysql:host=$host;dbname=$database, $username, $password");
+    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $erro) {
+    // Em caso de erro roda o que tem aqui
+    echo "Nao foi possivel conectar como banco de dados, motivo: " . $erro;
+}
